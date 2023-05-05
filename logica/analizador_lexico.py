425d71c9d578714c90706d548507af9c24855d88
jazmin saleh peña <jazminasaleh@gmail.com>
2023-05-05 17:16:40 -0500

    analisi estructural

--- logica/analizador_lexico.py
+++ logica/analizador_lexico.py
@@ -1,16 +1,25 @@
 import re
 #Analizador lexico
 class AnalizadorLexico:
-
+    #Constructor
     def __init__(self):
         self.letras = "^[a-zA-Z][^\\u00C0-\\u017F]*$"
         self.numeros = "^[0-9]+$"
         self.espacio = "^[ ]+$"
         self.guion = "^[\-]$"
-        #falta el -
-      
-    def analizar(self, codigoPostal):
+    
+    #coidgo sin carcateres invalidos
+    def codigoSinCarcateres(self, codigoPostal):
         er = r"^[a-zA-Z0-9-\s]+$"
+        cadena = ''
+        for i in range(len(codigoPostal)):
+            if re.findall(er, codigoPostal[i]):
+                cadena += codigoPostal[i]
+        return cadena
+    
+    #Tokenizacion, de acuerdo si es letra, numero espacio o guion
+    def analisisLexico(self, codigoPostal):
+        er = r"(^[a-zA-Z0-9-\s]+$)"
         filtrar = []
         for i in range(len(codigoPostal)):
             if re.findall(er, codigoPostal[i]):
@@ -19,17 +28,15 @@ class AnalizadorLexico:
                 print("La entrada %s no es valida y será retirada" % codigoPostal[i])
         
         tokens =[]
-        
         for i in filtrar:
             if re.match(self.letras,i):
                 tokens.append({f'{i}':'letra'})
             elif re.match(self.numeros,i):
                 tokens.append({f'{i}':'numero'})
-            elif re.match(self.espacio,i):
-                tokens.append({f'{i}':'espacio'})
             elif re.match(self.guion,i):
                 tokens.append({f'{i}':'guion'})
+            elif re.match(self.espacio,i):
+                tokens.append({f'{i}':'espacio'})
             else:
                 tokens.append({f'{i}':'invalido'})
         return tokens
-
--- logica/logica.py
+++ logica/logica.py
@@ -5,37 +5,116 @@ import json, re
 from logica.analizador_lexico import AnalizadorLexico
 #depenidndo del codigo se dirge a X pantalla
 def prueba(codigo):
-   
-    #consumo de la api
-    print('___CONSUMO API___\n')
-    print(infoapi(codigo))
     
-    #analizador lexico
-    print('Analizador lexico\n')
+    #Codigo sin carcateres
+    print('___codigo sin carcteres epseciales___\n')
     objeto = AnalizadorLexico();
-    resultado = objeto.analizar(codigo)
+    resultado = objeto.codigoSinCarcateres(codigo)
+    print(resultado)
+    
+     #analizador lexico
+    print('Analizador lexico\n')
+    analisisLexico = objeto.analisisLexico(codigo)
+    print(analisisLexico)
+   
+    
+     #consumo de la api
+    print('___CONSUMO API___\n')
+    print(infoapi(resultado))
     
     #tokenizacion
     print('___RESULTADO___\n')
-    print(resultado)
+   
     
     #cadena con tokens
-    for i in resultado:
+    for i in analisisLexico:
         print(i)
         
     #valores
     print('\n___KEYS___\n')
-    for i in resultado:
+    for i in analisisLexico:
         print(list(i.keys()))
    
-    #tokens
-    print('\n___VALOR___\n')
-    for i in resultado:
-        print(list(i.values()))
+    #Analisis estructural
+    print('\n___ANALISI ESTRUCTURAL___\n')
+    estructura = ''
+    contador = 0
+    contLetras =0
+    contNumeros = 0
+    contEspacios = 0
+    contGuiones = 0
+    for i in analisisLexico:
+        if(list(i.values()) == ['numero']):
+            if(contador == 0):
+                estructura += 'El primer digito es un número,'
+            else:
+                contNumeros +=  1
+                if(contLetras != 0):
+                    estructura += ' seguido de '+ str(contLetras) +' letras,'
+                    contLetras = 0
+                elif(contador == len(analisisLexico)-1):
+                    estructura += ' terminado en ' + str(contNumeros) +' números.'
+                elif(contEspacios !=0):
+                    estructura += ' seguido de '+ str(contEspacios) +' espacios,'
+                    contEspacios = 0
+                elif(contGuiones !=0):
+                    estructura += ' seguido de '+ str(contGuiones) +' guiones,'
+                    contGuiones = 0
+        elif(list(i.values()) == ['letra']):
+            if(contador == 0):
+                estructura += 'El primer digito es una letra,'
+            else:
+                contLetras += 1
+                if(contNumeros != 0):     
+                    estructura += ' seguido de '+ str(contNumeros) +' números,'
+                    contNumeros = 0
+                elif(contador == len(analisisLexico)-1):
+                    estructura += ' terminado en '+ str(contLetras) +' letras.'
+                elif(contEspacios !=0):
+                    estructura += ' seguido de '+ str(contEspacios) +' espacios,'
+                    contEspacios = 0
+                elif(contGuiones !=0):
+                    estructura += ' seguido de '+ str(contGuiones) +' guiones,'
+                    contGuiones = 0
+        elif(list(i.values()) == ['guion']):
+            if(contador == 0):
+                estructura += 'El primer digito es un guion,'
+            else:
+                contGuiones += 1
+                if(contLetras != 0):
+                    estructura += ' seguido de '+ str(contLetras) +' letras,'
+                    contLetras = 0
+                elif(contador == len(analisisLexico)-1):
+                    estructura += ' terminado en '+ str(contGuiones) +' guion'
+                elif(contEspacios !=0):
+                    estructura += ' seguido de '+ str(contEspacios) +' espacios,'
+                    contEspacios = 0
+                elif(contNumeros !=0):
+                    estructura += ' seguido de '+ str(contNumeros) +' números,'
+                    contNumeros = 0
+               
+        elif(list(i.values()) == ['espacio']):
+            if(contador == 0):
+                estructura += 'El primer digito es un espacio,'
+            else:
+                contEspacios += 1
+                if(contLetras != 0):
+                    estructura += ' seguido de '+ str(contLetras) +' letras,'
+                    contLetras = 0
+                elif(contador == len(analisisLexico)-1):
+                    estructura += ' terminado en '+ str(contEspacios) +' espacio'
+                elif(contGuiones !=0):
+                    estructura += ' seguido de '+ str(contGuiones) +' guiones,'
+                    contGuiones = 0
+                elif(contNumeros !=0):
+                    estructura += ' seguido de '+ str(contNumeros) +' números,'
+                    contNumeros = 0
+        contador += 1
+    print(estructura)
     
     #Dirigir a X pagina segun el codigo postal, si este es valido o no
-    if codigo != '000000': 
-        return redirect(f"https://www.google.com/maps/place/{codigo}/")
+    if infoapi(resultado) != '': 
+        return redirect(f"https://www.google.com/maps/place/{resultado}/")
     else:
         return render_template('error.html')
     
@@ -47,4 +126,7 @@ def infoapi(codigo):
     url = f'http://api.geonames.org/postalCodeSearchJSON?postalcode={codigo}&username=sebastiancorrea13'
     respuesta = urlopen(url)
     data = json.loads(respuesta.read())
-    return data
+    dataa = data["postalCodes"]
+    if(len(dataa) >= 1):
+        return dataa[0]['placeName'] 
+    return ''
